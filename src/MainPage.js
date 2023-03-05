7d8cbc7276cc6b87222de1552eb67b6ba5eae095
Jagpreet Grewal <jsg25@sfu.ca>
2023-03-04 20:37:49 -0800

    second page has buttons now

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,64 @@
 import React from "react";
+import example1Output from "./example1Output.txt";
+import "./MainPage.css";
 
-function MainPage() {
-  return <h1>Main Page</h1>;
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      example1Output: "",
+      example2Output: "",
+      example3Output: ""
+    };
+  }
+
+  handleExample1Click = () => {
+    // Use fetch to call example1.py and get its output
+    fetch("example1.py")
+      .then(response => response.text())
+      .then(output => this.setState({ example1Output: output }));
+  };
+
+  handleExample2Click = () => {
+    // Use fetch to call example2.py and get its output
+    fetch("example2.py")
+      .then(response => response.text())
+      .then(output => this.setState({ example2Output: output }));
+  };
+
+  handleExample3Click = () => {
+    // Use fetch to call example3.py and get its output
+    fetch("example3.py")
+      .then(response => response.text())
+      .then(output => this.setState({ example3Output: output }));
+  };
+
+  render() {
+    return (
+      <div className="MainPage">
+        <div className="SettingsColumn">
+          <button>Setting 1</button>
+          <button>Setting 2</button>
+          <button>Setting 3</button>
+          <button>Setting 4</button>
+          <button>Setting 5</button>
+        </div>
+        <div className="ScriptColumn">
+          <img src="image.png" alt="Script Image" className="ScriptImage" />
+          <div className="ScriptButtons">
+            <button onClick={this.handleExample1Click}>Example 1</button>
+            <button onClick={this.handleExample2Click}>Example 2</button>
+            <button onClick={this.handleExample3Click}>Example 3</button>
+          </div>
+          <div className="OutputBox">{this.state.example1Output}</div>
+        </div>
+        <div className="OutputColumn">
+          <div className="OutputBox">{this.state.example2Output}</div>
+          <div className="OutputBox">{this.state.example3Output}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
