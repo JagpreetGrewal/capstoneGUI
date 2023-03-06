17d98357ece1c9df20d5c389e721483b346bb04d
Jagpreet Grewal <jsg25@sfu.ca>
2023-03-05 17:26:29 -0800

    launchscript.js no longer being used net displaying on second page still need to launch scripts

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,46 +1,65 @@
-import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import LaunchScriptButton from './LaunchScriptButton';
+import React from "react";
+import example1Output from "./example1Output.txt";
+import "./MainPage.css";
+import Net from "./Net.png"
 
-const MainPage = () => {
-  return (
-    <Container fluid>
-      <Row>
-        <Col sm={2} className="bg-dark text-white p-3">
-          <h3>Settings</h3>
-          <Button variant="light" className="my-2">
-            Option 1
-          </Button>
-          <Button variant="light" className="my-2">
-            Option 2
-          </Button>
-          <Button variant="light" className="my-2">
-            Option 3
-          </Button>
-          <Button variant="light" className="my-2">
-            Option 4
-          </Button>
-          <Button variant="light" className="my-2">
-            Option 5
-          </Button>
-        </Col>
-        <Col sm={4} className="p-3">
-          <img src="image.jpg" className="img-fluid rounded-top" alt="example" />
-          <LaunchScriptButton />
-          <LaunchScriptButton />
-          <LaunchScriptButton />
-          <div className="mt-3 bg-light" style={{ height: '30vh' }}>
-            <p>Output goes here</p>
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      example1Output: "",
+      example2Output: "",
+      example3Output: ""
+    };
+  }
+
+  handleExample1Click = () => {
+    // Use fetch to call example1.py and get its output
+    fetch("example1.py")
+      .then(response => response.text())
+      .then(output => this.setState({ example1Output: output }));
+  };
+
+  handleExample2Click = () => {
+    // Use fetch to call example2.py and get its output
+    fetch("example2.py")
+      .then(response => response.text())
+      .then(output => this.setState({ example2Output: output }));
+  };
+
+  handleExample3Click = () => {
+    // Use fetch to call example3.py and get its output
+    fetch("example3.py")
+      .then(response => response.text())
+      .then(output => this.setState({ example3Output: output }));
+  };
+
+  render() {
+    return (
+      <div className="MainPage">
+        <div className="SettingsColumn">
+          <button>Setting 1</button>
+          <button>Setting 2</button>
+          <button>Setting 3</button>
+          <button>Setting 4</button>
+          <button>Setting 5</button>
+        </div>
+        <div className="ScriptColumn">
+          <img src={Net} alt="Script Image" className="ScriptImage" />
+          <div className="ScriptButtons">
+            <button onClick={this.handleExample1Click}>Example 1</button>
+            <button onClick={this.handleExample2Click}>Example 2</button>
+            <button onClick={this.handleExample3Click}>Example 3</button>
           </div>
-        </Col>
-        <Col sm={6} className="p-3">
-          <div className="bg-light" style={{ height: '80vh' }}>
-            <p>Output from scripts go here</p>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+          <div className="OutputBox">{this.state.example1Output}</div>
+        </div>
+        <div className="OutputColumn">
+          <div className="OutputBox">{this.state.example2Output}</div>
+          <div className="OutputBox">{this.state.example3Output}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default MainPage;
