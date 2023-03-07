5c30ea1e5086f428840bc4110968bc1ecf1ad61a
Jagpreet Grewal <jsg25@sfu.ca>
2023-03-06 16:34:40 -0800

    can stream opencv to flask

--- src/app.py
+++ src/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, Response
 import subprocess
-
-app = Flask(__name__)
+import cv2
+import numpy as np
 
 # run using $ python3 -m flask run
+
+app = Flask(__name__)
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+camera = cv2.VideoCapture(0)
+
+def gen():
+    while True:
+        temp, frame = camera.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        boxes, weights = hog.detectMultiScale(frame,winStride=(8, 8), padding=(4, 4),scale=1.05)
+        for (x, y, w, h) in boxes:
+            pad_w, pad_h = int(0.15 * w), int(0.01 * h)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/example1')
 def example1():
@@ -25,6 +44,10 @@ def example3():
     # output at http://127.0.0.1:5000/example1
     output = subprocess.check_output(['python3', 'example3.py'])
     return output
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run()
